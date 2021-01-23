6387b18c0762ecb9585d5bfd7ab18d80aaa3c922
barjoco <adambartonjones@gmail.com>
2021-01-23 19:43:42 +0000

    updated

--- tint2/exec/updates.sh
+++ tint2/exec/updates.sh
@@ -5,6 +5,5 @@ echo >&2 "Updates available ($(eopkg lu | wc -l))"
 if [ "$(eopkg lu)" = 'No packages to upgrade.' ]; then
     echo
 else
-    echo >&2 "Updates available ($(eopkg lu | wc -l))"
     echo 
 fi
