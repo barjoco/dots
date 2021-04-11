bee7a0821c6e525bef9c2c8bfd6717ede50f923d
Adam <adambartonjones@gmail.com>
2021-04-11 23:42:53 +0100

    updated

---
+++ script/emoji/emoji.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# Get list of emojis
+#
+# Navigate to https://unicode.org/emoji/charts/full-emoji-list.html
+# Open console and run:
+#
+# const n = document.getElementsByClassName('name');
+# const c = document.getElementsByClassName('chars');
+# let out;
+#
+# Array.from(c).forEach((v, i) => {
+#   out += `${v.innerText} ${n[i].innerText}<br>`
+# });
+#
+# document.body.innerHTML = out;
+
+e=$(cat $HOME/.config/scripts/emoji/emojis.txt | rofi -dmenu -i -p '')
+awk '{print $1}' <<< $e | tr -d '\n' | xclip -selection c
